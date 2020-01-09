10362174ddb8c74a6a3b2df87fd3a82249e17796
Luke Johnston <lwjohnst@gmail.com>
2020-01-09 21:46:04 +0100

    add project doc R file to then generate namespace

---
+++ R/ecc.cmd.ukbiobank-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @import dplyr vroom
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
