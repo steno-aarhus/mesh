d3df68835166a16ea745cc9de7c27a3f3c60dba1
Luke Johnston <lwjohnst@gmail.com>
2020-09-16 19:35:56 +0200

    update analysis of nc functions

--- R/netcoupler-analysis.R
+++ R/netcoupler-analysis.R
@@ -1,36 +1,38 @@
 
-analyze_nc_network <- function(.tbl) {
-    metabolic_network <- .tbl %>%
-        select(starts_with("mtb_")) %>%
-        NetCoupler::nc_create_network()
-
-    usethis::use_data(metabolic_network, overwrite = TRUE)
-    return(metabolic_network)
-}
-
-analyze_nc_outcome <- function(.tbl, .network) {
-    model_outcome_estimates <- .tbl %>%
-        NetCoupler::nc_outcome_estimates(
-            .network,
-            .outcome = "hba1c",
-            .adjustment_vars = c("age", "sex", "body_mass_index"),
-            .model_function = lm
+analyze_nc_standardize_mtb_vars <- function(.tbl) {
+    .tbl %>%
+        dplyr::mutate(sex_number = as.numeric(as.factor(sex))) %>%
+        NetCoupler::nc_standardize(
+            .vars = NetCoupler::starts_with("mtb_"),
+            # TODO: Compare with BMI
+            .regressed_on = c("age", "sex_number", "waist_circumference")
         )
-
-    usethis::use_data(model_outcome_estimates, overwrite = TRUE)
-    return(model_outcome_estimates)
 }
 
-analyze_nc_exposure <- function(.tbl, .network) {
-    model_exposure_estimates <- .tbl %>%
+analyze_nc_network <- function(.tbl) {
+    .tbl %>%
+        NetCoupler::nc_estimate_network(NetCoupler::starts_with("mtb_"))
+}
+
+analyze_nc_outcome <- function(.tbl, .network, .parallel = FALSE) {
+    .tbl %>%
         NetCoupler::nc_outcome_estimates(
-            .network,
+            NetCoupler::as_edge_tbl(.network),
+            .outcome = "hba1c",
+            .adjustment_vars = c("age", "sex", "waist_circumference"),
+            .model_function = lm,
+            .parallel = .parallel
+        )
+}
+
+analyze_nc_exposure <- function(.tbl, .network, .parallel) {
+    .tbl %>%
+        NetCoupler::nc_outcome_estimates(
+            NetCoupler::as_edge_tbl(.network),
             .outcome = "leg_height_ratio",
-            .adjustment_vars = c("age", "sex", "body_mass_index"),
-            .model_function = lm
+            .adjustment_vars = c("age", "sex", "waist_circumference"),
+            .model_function = lm,
+            .parallel = .parallel
         ) %>%
         rename(exposure = outcome)
-
-    usethis::use_data(model_exposure_estimates)
-    return(model_exposure_estimates)
 }
