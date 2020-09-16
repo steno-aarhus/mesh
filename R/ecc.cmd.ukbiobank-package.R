246b66ba88d3a9f63ea6631d0daa448371cc900b
Luke Johnston <lwjohnst@gmail.com>
2020-09-16 22:17:33 +0200

    don't need

--- R/ecc.cmd.ukbiobank-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-#' @import dplyr vroom
-"_PACKAGE"
-
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
-## usethis namespace: start
-## usethis namespace: end
-NULL
