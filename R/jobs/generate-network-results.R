9f8440204d6a98a5546ffd4f7e9c165ea04f8c04
Luke Johnston <lwjohnst@gmail.com>
2020-09-16 22:19:29 +0200

    progress bar and messages to job script

--- R/jobs/generate-network-results.R
+++ R/jobs/generate-network-results.R
@@ -1,11 +1,22 @@
-devtools::load_all()
+devtools::load_all(quiet = TRUE)
 library(furrr)
 library(NetCoupler)
 library(rsample)
-plan(multiprocess)
+library(progressr)
+
+plan(multiprocess, workers = availableCores() - 1)
+
+handlers(
+    handler_progress(
+        format = "[:bar] :percent",
+        width = 60,
+        enable = TRUE
+    )
+)
 
 set.seed(4125612)
 
+message("Preparing dataset for analysis.")
 proj_data <- prepare_data_for_netcoupler_analysis()
 
 proj_data_cv <- proj_data %>%
@@ -13,15 +24,21 @@ proj_data_cv <- proj_data %>%
     create_cv_splits()
 
 process_and_analyze <- function(.tbl) {
+    p()
     .tbl %>%
         as.data.frame() %>%
         analyze_nc_standardize_mtb_vars() %>%
         analyze_nc_network()
 }
 
-network_results <- proj_data_cv$splits %>%
-    future_map(process_and_analyze)
+message("Starting analysis.")
+with_progress({
+    p <- progressor(along = 1:length(proj_data_cv$splits))
+    network_results <- proj_data_cv$splits %>%
+        future_map(process_and_analyze)
+}, enable = TRUE)
+
+message("Ended analysis.")
+plan(sequential)
 
 usethis::use_data(network_results, overwrite = TRUE)
-
-plan(sequential)
