6b27561f09d1e4ae4c291ebfed10779e6df19090
Luke Johnston <lwjohnst@gmail.com>
2020-09-16 19:38:11 +0200

    add a bunch of functions to run rstudio jobs

--- R/run-jobs.R
+++ R/run-jobs.R
@@ -1,11 +1,34 @@
 
+run_generic_job <- function(.job_filename, .job_title, .export = "") {
+    rstudioapi::jobRunScript(
+        path = here::here("R", "jobs", .job_filename),
+        name = .job_title,
+        workingDir = here::here("."),
+        exportEnv = .export
+    )
+}
+
 # This is only necessary for slower computers.
 # On my computer `load_data()` on its own runs pretty fast.
-load_data_as_job <- function() {
-    rstudioapi::jobRunScript(
-        path = here::here("scripts/loading-project-data.R"),
-        name = "Load UK Biobank Project Data",
-        workingDir = here::here("."),
-        exportEnv = "R_GlobalEnv"
+run_job_load_data <- function() {
+    run_generic_job(
+        "loading-project-data.R",
+        "Load UK Biobank Project Data",
+        "R_GlobalEnv"
+    )
+}
+
+run_job_loading_raw <- function() {
+    run_generic_job(
+        "loading-raw.R",
+        "Loading original raw UK Biobank data",
+        "R_GlobalEnv"
+    )
+}
+
+run_job_network_results <- function() {
+    run_generic_job(
+        "generate-network-results.R",
+        "Generating the network results of the CV splits"
     )
 }
