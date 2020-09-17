92f8bbbb39a327e8f38b5f92ae249d78e107995d
Luke Johnston <lwjohnst@gmail.com>
2020-09-17 16:12:33 +0200

    update jobs to include furrr

--- R/jobs/generate-exposure-estimate-results.R
+++ R/jobs/generate-exposure-estimate-results.R
@@ -3,8 +3,11 @@ library(pcalg)
 library(NetCoupler)
 library(rsample)
 library(progress)
+library(furrr)
 
 set.seed(4125612)
+
+plan(multiprocess, workers = availableCores() - 1)
 
 message("Preparing dataset for analysis.")
 proj_data <- prepare_data_for_netcoupler_analysis()
@@ -13,36 +16,43 @@ proj_data_cv <- proj_data %>%
     training() %>%
     create_cv_splits()
 
-process_and_analyze <- function(.tbl, .network, .stature_var) {
-    pb$tick()
-    # TODO: Test parallel again but move this code out.
-    std_data <- .tbl %>%
+process_data <- function(.tbl) {
+    .tbl %>%
         as.data.frame() %>%
         NetCoupler::nc_standardize(NetCoupler::starts_with("mtb_"))
+}
 
+analyze_data <- function(.tbl, .network, .stature_var) {
+    pb$tick()
     analyze_nc_exposure(
-        .tbl = std_data,
+        .tbl = .tbl,
         .network = .network,
         .stature_var = .stature_var
     )
 }
 
 message("Starting analysis.")
-pb <- progress_bar$new(total = length(proj_data_cv$splits) * 4)
+num_splits <- length(proj_data_cv$splits)
+pb <- progress_bar$new(total = num_splits * 4)
 # Need to run generate-network-results.R first to get network_results
-exposure_estimate_results <- map(
-    c("standing_height",
-      "sitting_height",
-      "leg_length",
-      "leg_height_ratio"),
-    ~ map2(proj_data_cv$splits,
-           network_results,
-           process_and_analyze,
-           .stature_var = .x)
-) %>%
-    flatten() %>%
-    bind_rows(.id = "model_run_number")
+split_data <- proj_data_cv$splits %>%
+    map(process_data)
 
+stature_vars <- rep(c(
+    "standing_height",
+    "sitting_height",
+    "leg_length",
+    "leg_height_ratio"
+    ),
+    each = num_splits)
+
+exposure_estimate_results <-
+    list(
+        .tbl = split_data,
+        .network = network_results,
+        .stature_var = stature_vars
+    ) %>%
+    future_pmap_dfr(analyze_data, .id = "model_run_number")
 
 message("Ended analysis.")
 usethis::use_data(exposure_estimate_results, overwrite = TRUE)
--- R/jobs/generate-outcome-estimate-results.R
+++ R/jobs/generate-outcome-estimate-results.R
@@ -3,8 +3,11 @@ library(pcalg)
 library(NetCoupler)
 library(rsample)
 library(progress)
+library(furrr)
 
 set.seed(4125612)
+
+plan(multiprocess, workers = availableCores() - 1)
 
 message("Preparing dataset for analysis.")
 proj_data <- prepare_data_for_netcoupler_analysis()
@@ -13,15 +16,17 @@ proj_data_cv <- proj_data %>%
     training() %>%
     create_cv_splits()
 
-process_and_analyze <- function(.tbl, .network) {
-    pb$tick()
-    # TODO: Test parallel again but move this code out.
-    std_data <- .tbl %>%
+process_data <- function(.tbl) {
+    .tbl %>%
         as.data.frame() %>%
         NetCoupler::nc_standardize(NetCoupler::starts_with("mtb_"))
+}
 
+analyze_data <- function(.tbl, .network) {
+    # TODO: Test parallel again but move this code out.
+    pb$tick()
     analyze_nc_outcome(
-        .tbl = std_data,
+        .tbl = .tbl,
         .network = .network
     )
 }
@@ -29,12 +34,11 @@ process_and_analyze <- function(.tbl, .network) {
 message("Starting analysis.")
 pb <- progress_bar$new(total = length(proj_data_cv$splits))
 # Need to run generate-network-results.R first to get network_results
-outcome_estimate_results <- map2_dfr(
-    proj_data_cv$splits,
-    network_results,
-    process_and_analyze,
-    .id = "model_run_number"
-)
+outcome_estimate_results <- proj_data_cv$splits %>%
+    map(process_data) %>%
+    future_map2_dfr(network_results,
+                    process_and_analyze,
+                    .id = "model_run_number")
 
 message("Ended analysis.")
 usethis::use_data(outcome_estimate_results, overwrite = TRUE)
