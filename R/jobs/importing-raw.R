145928b1288892c2dc066de1ec4b41a18407f242
Luke W. Johnston <lwjohnst@gmail.com>
2021-12-15 23:08:19 +0100

    rename to import, not load

--- R/jobs/importing-raw.R
+++ R/jobs/importing-raw.R
@@ -1,3 +1,3 @@
 # To use for local job to keep session available.
 devtools::load_all(here::here("."))
-ukb_data_raw <- import_data_with_specific_columns()
+ukb_data_raw <- ukb_import_with_specific_columns()
