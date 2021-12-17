cf70e468b167be11ce4672c513e8d247204e2f9e
Luke W. Johnston <lwjohnst@gmail.com>
2021-12-17 13:11:28 +0100

    add descriptive statistics function

---
+++ R/analysis-descriptive.R
@@ -0,0 +1,23 @@
+
+analysis_descriptive_statistics <- function(data) {
+    descriptive_statistics <- list(
+        full_study = skimr::skim(data),
+        by_sex = data %>%
+            dplyr::group_by(sex) %>%
+            skimr::skim() %>%
+            dplyr::ungroup(),
+        full_study_chr = data %>%
+            dplyr::select(dplyr::where(is.character)) %>%
+            tidyr::pivot_longer(dplyr::everything()) %>%
+            dplyr::count(name, value)
+        by_sex_chr = data %>%
+            dplyr::group_by(sex) %>%
+            dplyr::select(dplyr::where(is.character)) %>%
+            tidyr::pivot_longer(-sex) %>%
+            dplyr::count(name, value) %>%
+            dplyr::ungroup()
+    )
+
+    usethis::use_data(descriptive_statistics, overwrite = TRUE)
+    return(here::here("data/descriptive_statistics.rda"))
+}
