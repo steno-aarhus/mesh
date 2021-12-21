57afc547a62e0c6d42c57dcfc64f30796cc4ca27
Luke W. Johnston <lwjohnst@gmail.com>
2021-12-22 00:18:08 +0100

    start adding group by sex settings to function

--- R/analysis-netcoupler.R
+++ R/analysis-netcoupler.R
@@ -1,17 +1,23 @@
 
-analyze_nc_standardize_mtb_vars <- function(.tbl) {
-    .tbl %>%
+analyze_nc_standardize_mtb_vars <- function(data, group_by_sex = FALSE) {
+    # TODO: Do this as a global variable instead?
+    confounders <- c("age", "sex_number", "waist_circumference")
+    if (group_by_sex) {
+        confounders <- stringr::str_subset(confounders, "^sex_number$", negate = TRUE)
+    }
+
+    data %>%
+        {if (group_by_sex) dplyr::group_by(sex) else .} %>%
         dplyr::mutate(sex_number = as.numeric(as.factor(sex))) %>%
         NetCoupler::nc_standardize(
-            cols = NetCoupler::starts_with("mtb_"),
+            cols = dplyr::starts_with("mtb_"),
             # TODO: Compare with BMI
-            regressed_on = c("age", "sex_number", "waist_circumference")
+            regressed_on = confounders
         )
 }
 
 analyze_nc_network <- function(data) {
-    data %>%
-        NetCoupler::nc_estimate_network(NetCoupler::starts_with("mtb_"))
+    NetCoupler::nc_estimate_network(data, dplyr::starts_with("mtb_"))
 }
 
 analyze_nc_outcome <- function(data, .network) {
--- R/generate-results.R
+++ R/generate-results.R
@@ -1,12 +1,12 @@
-generate_network_results <- function(tbl_as_cv_splits) {
-    process_and_analyze <- function(tbl) {
-        tbl %>%
+generate_network_results <- function(data_as_cv_splits, group_by_sex = FALSE) {
+    process_and_analyze <- function(data, group_by_sex) {
+        data %>%
             as.data.frame() %>%
             analyze_nc_standardize_mtb_vars() %>%
             analyze_nc_network()
     }
 
-    network_results <- tbl_as_cv_splits %>%
+    network_results <- data_as_cv_splits %>%
         purrr::map(process_and_analyze)
 
     usethis::use_data(network_results, overwrite = TRUE)
