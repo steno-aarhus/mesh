881a3c79bce507503174710e854c88d33b60f11e
Luke W. Johnston <lwjohnst@gmail.com>
2022-02-07 17:58:14 +0100

    expand on group by sex in netcoupler

--- R/generate-results.R
+++ R/generate-results.R
@@ -1,13 +1,13 @@
-generate_network_results <- function(data_as_cv_splits, group_by_sex = FALSE) {
+generate_network_results <- function(data_as_cv_splits) {
     process_and_analyze <- function(data, group_by_sex) {
         data %>%
             as.data.frame() %>%
-            analyze_nc_standardize_mtb_vars() %>%
-            analyze_nc_network()
+            analyze_nc_standardize_mtb_vars(group_by_sex = group_by_sex) %>%
+            analyze_nc_network(group_by_sex = group_by_sex)
     }
 
-    network_results <- data_as_cv_splits %>%
-        purrr::map(process_and_analyze)
+    network_results_total <- data_as_cv_splits %>%
+        purrr::map(process_and_analyze, group_by_sex = FALSE)
 
     usethis::use_data(network_results, overwrite = TRUE)
     return(here::here("data/network_results.rda"))
